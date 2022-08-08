1ccf54050275e6328521e04171ccdf37d29a903e
mehdi-azrabi <mr.azrabi@gmail.com>
2022-08-08 23:06:14 +1000

    sec hello commit

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,15 @@ print(1+2)
 print(7*6)
 print()
 print("The end", "or is it ?", "keep watching to lear more about python", 3)
+
+print("Today is a good day to lear python")
+print('python is fun')
+print("python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+#if we want a space we can add that too
+print(greeting + ' ' +  name)
+
