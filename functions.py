3a2628e3bcf22a5ff9d2305c0cc5cbae41b83d22
mehdi-azrabi <mr.azrabi@gmail.com>
2022-08-14 23:08:41 +1000

    comit

--- functions.py
+++ functions.py
@@ -9,8 +9,18 @@ def is_palindrome(string):
     return string[::-1].casefold() == string.casefold()
 
 
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+    print(string)
+    # return string[::-1].casefold() == string.casefold()
+    return is_palindrome(string)
+
+
 word = input("Please enter a word to check : ")
-if is_palindrome(word):
+if palindrome_sentence(word):
     print("'{}' is a palindrome".format(word))
 else:
     print("'{}' is not a palindrome".format(word))
