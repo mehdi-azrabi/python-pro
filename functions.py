0d80f355eb639cb8908314845b25842d208ab097
mehdi-azrabi <mr.azrabi@gmail.com>
2022-08-14 22:17:39 +1000

    commit fun

--- functions.py
+++ functions.py
@@ -1,9 +1,16 @@
-def multiply():
-    result = 10.5 * 4
+def multiply(x, y):
+    result = x * y
     return result
 
 
-answer = multiply()
-print(answer)
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].casefold() == string.casefold()
 
-print(multiply())
+
+word = input("Please enter a word to check : ")
+if is_palindrome(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
